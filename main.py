6a612fb698c9001db3f0623e2671544be0e78b71
Ali Bridgers <abridgers@gmail.com>
2023-04-02 11:42:07 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Module 3 Challenge
+#Analyze budget_data.csv. Dataset contains two columns: Date (day-month) and Profit/Losses (large numbers)
+
+#import modules
+import os
+import csv
+
+#open the file
+with open('..')
